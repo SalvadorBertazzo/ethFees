307cd1dc6d8a122f83e168dec16fd4474440dd8e
Salvador Bertazzo <salvabertazzo@gmail.com>
2025-06-28 22:27:01 -0300

    Update .env file loading error handling to a warning message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Warning: .env file not found, using system environment variables")
 	}
 
 	server := api.NewServer()
